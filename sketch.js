3a05c1db47a0ebca07c5d2125f1f84cb94efc6ee
Chaitanya Purohit <72564530+Chaitanya-purohit@users.noreply.github.com>
2021-10-29 17:14:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var tieFighter, x_Wing, tieSound, x_WingSound
+
+
+function preload(){
+tieFighter = loadImage("TieFighter.png")
+x_Wing = loadImage("X-wing.png")
+tieSound = loadSound("tieFighterSound.mp3")
+x_WingSound = loadSound("xWingSound.mp3")
+}
+
+
+
+function setup() {
+  createCanvas(1600, 400);
+
+  
+  
+	
+	
+	
+	
+}
+
+
+function draw() {
+  background(0);  
+ 
+ 
+
+
+  drawSprites();
+ 
+}
+
+
